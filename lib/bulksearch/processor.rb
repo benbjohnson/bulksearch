41c3f06a630f40375d9f412bbb57c49a120b2f35
Ben Johnson <benbjohnson@yahoo.com>
2013-01-22 14:56:09 -0700

    Fix error on no URLs returned.

--- lib/bulksearch/processor.rb
+++ lib/bulksearch/processor.rb
@@ -44,55 +44,67 @@ class BulkSearch
       
       # Read the headers.
       headers = sheet.row(0).map { |cell| cell.to_s }
-      
-      # Loop over the remaining rows and 
-      sheet.each(1) do |row|
-        # Determine base options.
-        options = {}
-        headers.each_with_index do |header, index|
-          case header
-          when 'TERMS' then options[:terms] = row[index].to_s
-          when 'SITE' then options[:site] = row[index].to_s
+
+      begin
+        # Loop over the remaining rows and 
+        sheet.each(1) do |row|
+          # Determine base options.
+          options = {}
+          headers.each_with_index do |header, index|
+            case header
+            when 'TERMS' then options[:terms] = row[index].to_s
+            when 'SITE' then options[:site] = row[index].to_s
+            end
           end
-        end
         
-        # Perform searches on the rest of the columns.
-        headers.each_with_index do |header, index|
-          next unless header.index(/^[A-Z]+$/).nil?
+          # Perform searches on the rest of the columns.
+          headers.each_with_index do |header, index|
+            next unless header.index(/^[A-Z]+$/).nil?
           
-          # Combine search terms.
-          terms = "#{options[:terms].to_s} #{header}"
+            # Combine search terms.
+            terms = "#{options[:terms].to_s} #{header}"
           
-          # Only update the field if it's blank.
-          if row[index].to_s =~ /^\s*$/
-            # Process using Bing.
-            if providers.index(:bing)
-              puts "? #{terms} (#{options[:site]})"
-              search = bing_client.web.containing(terms)
-              search.from_site(options[:site]) unless options[:site].nil?
-              result = search.fetch.results[0..2].select {|result| URI.parse(result.url).path != '/' rescue false}.first
+            # Only update the field if it's blank.
+            if row[index].to_s =~ /^\s*$/
+              # Process using Bing.
+              if providers.index(:bing)
+                puts "? #{terms} (#{options[:site]})"
+                search = bing_client.web.containing(terms)
+                search.from_site(options[:site]) unless options[:site].nil?
+                result = search.fetch.results[0..2].select {|result| URI.parse(result.url).path != '/' rescue false}.first
 
-              puts "#{result.url}\n\n"
-              row[index] = result.url
-            end
+                if !result.nil?
+                  puts "#{result.url}\n\n"
+                  row[index] = result.url
+                else
+                  puts ""
+                end
+              end
 
-            # Process using Yahoo BOSS.
-            if providers.index(:boss)
-              puts "BOSS: #{terms}"
-              BOSSMan::Search.web(terms, :count => 1)
-              search = boss_client.web.containing(terms)
-              result = search.results.first
+              # Process using Yahoo BOSS.
+              if providers.index(:boss)
+                puts "BOSS: #{terms}"
+                BOSSMan::Search.web(terms, :count => 1)
+                search = boss_client.web.containing(terms)
+                result = search.results.first
 
-              puts "  \\-> #{result.url}\n\n"
-              row[index] = result.url
+                if !result.nil?
+                  puts "#{result.url}\n\n"
+                  row[index] = result.url
+                else
+                  puts ""
+                end
+              end
             end
           end
         end
-      end
       
-      # Backup original file & write over original file.
-      FileUtils.mv(file, "#{File.dirname(file)}/.#{File.basename(file)}")
-      book.write(file)
+      # If an error occurs, save off the file before exiting.
+      ensure
+        # Backup original file & write over original file.
+        FileUtils.mv(file, "#{File.dirname(file)}/.#{File.basename(file)}")
+        book.write(file)
+      end
     end
   end
 end
